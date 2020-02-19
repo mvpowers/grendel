1c573327e9cadd2b853357e3bdc2f3eef1da4a97
Mikey Powers <michaelvincentpowers@gmail.com>
2020-02-19 12:43:36 -0800

    add access token

--- deploy/launch.sh
+++ deploy/launch.sh
@@ -9,3 +9,7 @@ usermod -aG docker ubuntu
 # install docker-compose
 curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+
+# MAKE SURE TO COPY IN ACCESS TOKEN
+git clone https://mvpowers:{ACCESS_TOKEN}@github.com/mvpowers/grendel.git
+/bin/bash /grendel/deploy/copy_env.sh
