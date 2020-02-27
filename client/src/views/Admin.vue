44d143e47df1a4299cb5d292e19a875cacdc340c
Mikey Powers <michaelvincentpowers@gmail.com>
2020-02-27 12:23:40 -0800

    test jenkins

--- client/src/views/Admin.vue
+++ client/src/views/Admin.vue
@@ -6,7 +6,7 @@
         sm8>
         <VLayout row>
           <VFlex>
-            <h1>Admin Center</h1>
+            <h1>Extreme Admin Center</h1>
           </VFlex>
           <VFlex class="grow" />
           <VBtn @click="$router.back()">
